04f167d7a23bc64012d51eff0492d1daaea8e899
Eldo44 <eldom44@gmail.com>
2018-09-18 08:04:24 +0530

    Add Swagger 2 configuration and document REST API's
    Signed-off-by: Eldo44 <eldom44@gmail.com>

--- displaytweet/src/main/java/com/twitter/controller/TweetController.java
+++ displaytweet/src/main/java/com/twitter/controller/TweetController.java
@@ -4,16 +4,22 @@ import com.twitter.dto.TweetContentDTO;
 import com.twitter.model.Tweet;
 import com.twitter.oauth.TwitterClient;
 import com.twitter.repository.TweetRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Api(value = "Twitter", description = "Extracts and displays tweets")
 public class TweetController {
 
 
@@ -25,7 +31,15 @@ public class TweetController {
     @Autowired
     ModelMapper modelMapper;
 
-    @RequestMapping("/tweets")
+    @ApiOperation(value = "Displays last 10 tweets tracked on a keyword", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    @RequestMapping(value = "/tweets", method = RequestMethod.GET, produces = "application/json")
     public List<TweetContentDTO> getTweets() {
         List<Tweet> tweets = repository.findFirst10ByOrderByCreationDateDesc();
         return tweets.stream().map(this::convertToDto).collect(Collectors.toList());
--- displaytweet/src/main/java/com/twitter/dto/TweetContentDTO.java
+++ displaytweet/src/main/java/com/twitter/dto/TweetContentDTO.java
@@ -1,9 +1,12 @@
 package com.twitter.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * This class is created so that only message/tweet text is shown in the UI layer
  */
 public class TweetContentDTO {
+    @ApiModelProperty(notes = "The tweet text")
     private String messageText;
 
     public String getMessageText() {
